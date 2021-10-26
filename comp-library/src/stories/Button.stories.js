dde20acad59ebd96c26c357462ce52534b7c0106
Jazem <jazemf.s@gmail.com>
2021-10-25 19:55:21 -0700

    starter

---
+++ comp-library/src/stories/Button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+export default {
+  component: Button,
+  title: "Chakra/Button",
+};
+
+export const Success = () => <Button colorScheme="green">Success</Button>;
